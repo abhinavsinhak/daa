b7e30c606c0d5bafdd62a38844891c9a4c2660c4
abhinavsinhak <shuhantgaurav2005@gmail.com>
2023-08-22 04:21:37 +0530

    new

--- graph_colour.c
+++ graph_colour.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define V 4 // Number of vertices
+#define MAX_V 10 // Maximum number of vertices
 
-void printSolution(int color[]) {
+void printSolution(int color[], int V) {
     printf("Vertex Colors:\n");
     for (int i = 0; i < V; i++) {
         printf("Vertex %d: Color %d\n", i, color[i]);
     }
+    printf("\n");
 }
 
-bool isSafe(int graph[V][V], int vertex, int color[], int c) {
+bool isSafe(int graph[MAX_V][MAX_V], int vertex, int color[], int c, int V) {
     for (int i = 0; i < V; i++) {
         if (graph[vertex][i] && color[i] == c) {
             return false;
@@ -19,17 +20,16 @@ bool isSafe(int graph[V][V], int vertex, int color[], int c) {
     return true;
 }
 
-bool graphColoringUtil(int graph[V][V], int m, int color[], int vertex) {
+bool graphColoringUtil(int graph[MAX_V][MAX_V], int m, int color[], int vertex, int V) {
     if (vertex == V) {
+        printSolution(color, V);
         return true; // All vertices are colored
     }
 
     for (int c = 1; c <= m; c++) {
-        if (isSafe(graph, vertex, color, c)) {
+        if (isSafe(graph, vertex, color, c, V)) {
             color[vertex] = c;
-            if (graphColoringUtil(graph, m, color, vertex + 1)) {
-                return true;
-            }
+            graphColoringUtil(graph, m, color, vertex + 1, V);
             color[vertex] = 0; // Backtrack
         }
     }
@@ -37,31 +37,33 @@ bool graphColoringUtil(int graph[V][V], int m, int color[], int vertex) {
     return false;
 }
 
-bool graphColoring(int graph[V][V], int m) {
-    int color[V];
+void graphColoring(int graph[MAX_V][MAX_V], int m, int V) {
+    int color[MAX_V];
     for (int i = 0; i < V; i++) {
         color[i] = 0; // Initialize colors
     }
 
-    if (!graphColoringUtil(graph, m, color, 0)) {
-        printf("No solution exists.\n");
-        return false;
-    }
-
-    printSolution(color);
-    return true;
+    graphColoringUtil(graph, m, color, 0, V);
 }
 
 int main() {
-    int graph[V][V] = {
-        {0, 1, 1, 1},
-        {1, 0, 1, 0},
-        {1, 1, 0, 1},
-        {1, 0, 1, 0}
-    };
+    int V;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &V);
 
-    int m = 3; // Number of colors
-    graphColoring(graph, m);
+    int graph[MAX_V][MAX_V];
+    printf("Enter the adjacency matrix (0/1):\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    int m;
+    printf("Enter the number of colors: ");
+    scanf("%d", &m);
+
+    graphColoring(graph, m, V);
 
     return 0;
 }
--- tsp.c
+++ tsp.c
@@ -1,77 +1,72 @@
 #include <stdio.h>
-#include <limits.h>
-
-#define MAX_CITIES 10
-
-int min(int a, int b)
+int matrix[25][25], visited_cities[10], limit, cost = 0;
+ 
+int tsp(int c)
 {
-    return (a < b) ? a : b;
+ int count, nearest_city = 999;
+ int minimum = 999, temp;
+ for(count = 0; count < limit; count++)
+ {
+ if((matrix[c][count] != 0) && (visited_cities[count] == 0))
+ {
+ if(matrix[c][count] < minimum)
+ {
+ minimum = matrix[count][0] + matrix[c][count];
+ }
+ temp = matrix[c][count];
+ nearest_city = count;
+ }
+ }
+ if(minimum != 999)
+ {
+ cost = cost + temp;
+ }
+ return nearest_city;
 }
-
-int tsp(int graph[MAX_CITIES][MAX_CITIES], int n, int start)
+ 
+void minimum_cost(int city)
 {
-    int mask = (1 << n) - 1; 
-    int dp[MAX_CITIES][1 << MAX_CITIES];
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < (1 << n); j++)
-        {
-            dp[i][j] = INT_MAX;
-        }
-    }
-
-    dp[start][mask] = 0;
-
-    for (int maskSubset = mask; maskSubset >= 0; maskSubset--)
-    {
-        for (int u = 0; u < n; u++)
-        {
-            for (int v = 0; v < n; v++)
-            {
-                if (!(maskSubset & (1 << v)))
-                {
-                    dp[u][maskSubset] = min(dp[u][maskSubset], graph[u][v] + dp[v][maskSubset | (1 << v)]);
-                }
-            }
-        }
-    }
-
-    int minCost = INT_MAX;
-    for (int v = 0; v < n; v++)
-    {
-        if (v != start)
-        {
-            minCost = min(minCost, graph[start][v] + dp[v][1 << v]);
-        }
-    }
-
-    return minCost;
+ int nearest_city;
+ visited_cities[city] = 1;
+ printf("%d ", city + 1);
+ nearest_city = tsp(city);
+ if(nearest_city == 999)
+ {
+ nearest_city = 0;
+ printf("%d", nearest_city + 1);
+ cost = cost + matrix[city][nearest_city];
+ return;
+ }
+ minimum_cost(nearest_city);
 }
-
+ 
 int main()
-{
-    int n;
-    printf("Enter the number of cities: ");
-    scanf("%d", &n);
-
-    int graph[MAX_CITIES][MAX_CITIES];
-
-    printf("Enter the distance matrix:\n");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            scanf("%d", &graph[i][j]);
-        }
-    }
-
-    int startCity;
-    printf("Enter the starting city (0 to %d): ", n - 1);
-    scanf("%d", &startCity);
-
-    int minDistance = tsp(graph, n, startCity);
-    printf("Minimum distance: %d\n", minDistance);
-
-    return 0;
+{ 
+ int i, j;
+ printf("Enter Total Number of Cities:\t");
+ scanf("%d", &limit);
+ printf("\nEnter Cost Matrix\n");
+ for(i = 0; i < limit; i++)
+ {
+ printf("\nEnter %d Elements in Row[%d]\n", limit, i + 1);
+ for(j = 0; j < limit; j++)
+ {
+ scanf("%d", &matrix[i][j]);
+ }
+ visited_cities[i] = 0;
+ }
+ printf("\nEntered Cost Matrix\n");
+ for(i = 0; i < limit; i++)
+ {
+ printf("\n");
+ for(j = 0; j < limit; j++)
+ {
+ printf("%d ", matrix[i][j]);
+ }
+ }
+ printf("\n\nPath:\t");
+ minimum_cost(0);
+ printf("\n\nMinimum Cost: \t");
+ printf("%d\n", cost);
+ return 0;
 }
