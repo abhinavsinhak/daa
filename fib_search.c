e86297a49f68b7a7a13cf79c978cafc7efe206e4
abhinavsinhak <shuhantgaurav2005@gmail.com>
2023-09-07 11:06:28 +0530

    new

--- fib_search.c
+++ fib_search.c
@@ -8,7 +8,8 @@ int fibMonaccianSearch(int arr[], int x, int n)
     int fibMMm1 = 1;
     int fibM = fibMMm2 + fibMMm1;
 
-    while (fibM < n) {
+    while (fibM < n)
+    {
         fibMMm2 = fibMMm1;
         fibMMm1 = fibM;
         fibM = fibMMm2 + fibMMm1;
@@ -16,19 +17,25 @@ int fibMonaccianSearch(int arr[], int x, int n)
 
     int offset = -1;
 
-    while (fibM > 1) {
+    while (fibM > 1)
+    {
         int i = min(offset + fibMMm2, n - 1);
 
-        if (arr[i] < x) {
+        if (arr[i] < x)
+        {
             fibM = fibMMm1;
             fibMMm1 = fibMMm2;
             fibMMm2 = fibM - fibMMm1;
             offset = i;
-        } else if (arr[i] > x) {
+        }
+        else if (arr[i] > x)
+        {
             fibM = fibMMm2;
             fibMMm1 = fibMMm1 - fibMMm2;
             fibMMm2 = fibM - fibMMm1;
-        } else {
+        }
+        else
+        {
             return i;
         }
     }
@@ -41,7 +48,7 @@ int fibMonaccianSearch(int arr[], int x, int n)
 
 int main(void)
 {
-    int arr[] = { 10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100, 235 };
+    int arr[] = {10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100, 235};
     int n = sizeof(arr) / sizeof(arr[0]);
     int x = 235;
     int ind = fibMonaccianSearch(arr, x, n);
